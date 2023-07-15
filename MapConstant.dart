9d04b62da37001f2b0e5609b39db9ae627b4ce34
rmpooria <75690473+rmpooria@users.noreply.github.com>
2023-07-15 09:24:27 +0330

    Add files via upload

---
+++ MapConstant.dart
@@ -0,0 +1,9 @@
+import 'package:latlong2/latlong.dart';
+
+class AppConstants {
+  static const String mapBoxAccessToken = 'pk.eyJ1Ijoicm1wb29yaWEiLCJhIjoiY2xqcHE4M2VtMDFjcjNlcGR2NjYwNnU0dyJ9.G2fdNpoxSlf6FmliZYXxbw';
+
+  static const String mapBoxStyleId = 'https://api.mapbox.com/styles/v1/rmpooria/cljpqfw0y00vr01pdg2tw7kes/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoicm1wb29yaWEiLCJhIjoiY2xqcHE4M2VtMDFjcjNlcGR2NjYwNnU0dyJ9.G2fdNpoxSlf6FmliZYXxbw';
+
+  static final myLocation = LatLng(51.5090214, -0.1982948);
+}
